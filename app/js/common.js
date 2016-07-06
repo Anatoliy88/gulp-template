cfba0ddc3cb3aef24c476831db5607ab494a5eee
Anatoliy88 <rudolf2788@gmail.com>
2016-07-06 20:44:34 +0300

    directory stylus, add popup js

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,46 @@
-$(function(){
 
+// Forms modal
+
+var link = document.querySelector(".contact-button"),
+    popup = document.querySelector(".write-us-popup"),
+    close = document.querySelector(".close-popup"),
+    form = popup.querySelector("form"),
+    login = form.querySelector("[name=name]"),
+    mail = form.querySelector("[name=mail]"),
+    storageLogin = localStorage.getItem("login"),
+    storageMail = localStorage.getItem("mail");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("popup-show");
+
+  if(storageLogin) {
+    login.value = storageLogin;
+    mail.focus();
+  } else {
+    login.focus();
+  }
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("popup-show");
+});
+
+form.addEventListener("submit", function(event) {
+  if(!login.value||!mail.value) {
+    event.preventDefault();
+    popup.classList.remove("popup-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("popup-error");
+    }  else {
+      localStorage.setItem("login", login.value);
+      localStorage.setItem("mail", mail.value);
+  }
+});
+
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode == 27 && popup.classList.contains("popup-show")) {
+    popup.classList.remove("popup-show")
+  }
 });
